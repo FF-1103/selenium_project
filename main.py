f8daab57c0b46d3990db2494008cd907ac8c3b1b
FF <332021128@qq.com>
2020-08-19 17:19:59 +0800

    test

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+# 开发人员   ：Fanzhishuo
+# 开发时间   ：2020-08-19  16:24
+# 文件名称   ：test_user_login.PY
+# 开发工具   ：PyCharm
 from util import util
 from selenium import webdriver
 from testcases.basic.test_user_register import TestUserRegister
