f5db12003f63fb3a73b7cb6e35236faed8cc1855
FF <332021128@qq.com>
2020-08-20 11:02:25 +0800

    单位电脑 提交测试管理员异常登录用例

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ from util import util
 from selenium import webdriver
 from testcases.basic.test_user_register import TestUserRegister
 from testcases.basic.test_user_login import TestUserLogin
+from testcases.basic.test_admin_logiin import TestAdminLogin
 if __name__ == '__main__':
     # case01 = TestUserRegister()
     # case01.test_register_code_error()
     # case01.test_register_ok()
-    case02 = TestUserLogin()
+    # case02 = TestUserLogin()
     # case02.test_user_login_username_error()
-    case02.test_user_login_ok()
+    # case02.test_user_login_ok()
+    case03 = TestAdminLogin()
+    case03.test_admin_login_username_error()
--- testcases/basic/test_admin_logiin.py
+++ testcases/basic/test_admin_logiin.py
@@ -4,9 +4,34 @@
 # 文件名称   ：test_user_login.PY
 # 开发工具   ：PyCharm
 from selenium import webdriver
+from util import util
+from time import sleep
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class TestAdminLogin(object):
     def __init__(self):
         self.driver = webdriver.Chrome()
+        self.driver.get('http://localhost:8080/jpress/admin/login')
+        self.driver.maximize_window()
+
+    def test_admin_login_username_error(self):
+        username = ''
+        pwd = 'admin'
+        expected = '账号不能为空'
+
+        self.driver.find_element_by_xpath('//*[@id="form"]/div[1]/input').send_keys(username)
+        self.driver.find_element_by_xpath('//*[@id="form"]/div[2]/input').send_keys(pwd)
+        captcha = util.get_code(self.driver, 'captchaImg')
+        self.driver.find_element_by_xpath('//*[@id="form"]/div[3]/input').send_keys(captcha)
+        self.driver.find_element_by_xpath('//*[@id="form"]/div[4]/div/button').click()
+
+        WebDriverWait(self.driver, 5).until(EC.alert_is_present())
+        alert = self.driver.switch_to.alert
+        sleep(3)
+        print(alert.text)
+        assert alert.text == expected
+        alert.accept()
+
 
